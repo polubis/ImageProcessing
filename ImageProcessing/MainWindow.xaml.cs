db3f1d13fe5766219a3b4e52ee6954fbf63c61b9
polubis <polubis@wp.pl>
2019-03-04 18:34:17 +0100

    Adding prperlly counting

--- ImageProcessing/MainWindow.xaml.cs
+++ ImageProcessing/MainWindow.xaml.cs
@@ -58,35 +58,120 @@ namespace ImageProcessing
             return dictionary;
         }
 
-        public void CreateShadesCsvWithoutTreads(string path, string headerLine = "X,Y,R,G,B,A")
+        public void CreateShadesCsvWithoutThreads(string path, string headerLine = "X,Y,R,G,B,A")
         {
             Bitmap img = new Bitmap(path);
             var strBuilder = new StringBuilder();
             strBuilder.AppendLine(headerLine);
-            var dictionaryShades = InitializeDictionary(img.Width, img.Height);
-        
+            var rShades = InitializeDictionary(img.Width, img.Height);
+            var gShades = InitializeDictionary(img.Width, img.Height);
+            var bShades = InitializeDictionary(img.Width, img.Height);
+
             for (int i = 0; i < img.Width; i++)
             {
                 for (int j = 0; j < img.Height; j++)
                 {
                     System.Drawing.Color pixel = img.GetPixel(i, j);
                     strBuilder.AppendLine(String.Format("{0},{1},{2},{3},{4},{5}", i, j, pixel.R, pixel.G, pixel.B, pixel.A));
-                    dictionaryShades[pixel.R] = dictionaryShades[pixel.R] + 1;
+                    rShades[pixel.R] = rShades[pixel.R] + 1;
+                    gShades[pixel.G] = gShades[pixel.G] + 1;
+                    bShades[pixel.B] = bShades[pixel.B] + 1;
                 }
             }
 
             SaveCsv(path, strBuilder);
 
             var countsStrBuilder = new StringBuilder();
-            countsStrBuilder.AppendLine("RGBA COUNTS");
-            countsStrBuilder.AppendLine("Key, Value");
-
-            foreach (var el in dictionaryShades)
-            {
-                countsStrBuilder.AppendLine(String.Format("{0}, {1}", el.Key, el.Value));
-            }
+            countsStrBuilder.AppendLine("RGB COUNTS");
+            countsStrBuilder.AppendLine("RCounts, Value");
+            PrintShades(rShades, countsStrBuilder);
+            countsStrBuilder.AppendLine("GCounts, Value");
+            PrintShades(gShades, countsStrBuilder);
+            countsStrBuilder.AppendLine("BCounts, Value");
+            PrintShades(bShades, countsStrBuilder);
 
             SaveCsv(path, countsStrBuilder, "rgba.csv");
+        }
+
+        public void CreateShadesWithMultipleThreads(string path, int threadsNumber, string headerLine = "X,Y,R,G,B,A")
+        {
+            Bitmap img = new Bitmap(path);
+            var strBuilder = new StringBuilder();
+            strBuilder.AppendLine(headerLine);
+            var rShades = InitializeDictionary(img.Width, img.Height);
+            var gShades = InitializeDictionary(img.Width, img.Height);
+            var bShades = InitializeDictionary(img.Width, img.Height);
+
+            int widthJump = img.Width / threadsNumber;
+            int heightJump = img.Height / threadsNumber;
+
+            int moduloWidth = img.Width % threadsNumber;
+            int moduloHeight = img.Height % threadsNumber;
+
+            int[] widthBreakpoints = new int[threadsNumber+1];
+            int[] heightBreakpoints = new int[threadsNumber+1];
+            widthBreakpoints[0] = 0;
+            heightBreakpoints[0] = 0;
+
+            for (int i = 2; i < threadsNumber+2; i++)
+            {
+                widthBreakpoints[i - 1] = widthJump*(i-1);
+                heightBreakpoints[i - 1] = heightJump *(i-1);
+            }
+
+            if (moduloWidth != 0)
+            {
+                widthBreakpoints[threadsNumber] = widthBreakpoints[threadsNumber] + moduloWidth;
+            } 
+
+            if (moduloHeight != 0)
+            {
+                heightBreakpoints[threadsNumber] = heightBreakpoints[threadsNumber] + moduloHeight;
+            }
+
+
+            Parallel.For(0, threadsNumber,
+                index =>
+                {
+                    Bitmap clonedImage = (Bitmap)img.Clone();
+                    int widthStartIndex = widthBreakpoints[index];
+                    int heightStartIndex = heightBreakpoints[index];
+                    int widthLimit = widthBreakpoints[index+1];
+                    int heightLimit = heightBreakpoints[index+1];
+
+                    for (int i = widthStartIndex; i < widthLimit; i++)
+                    {
+                        for (int j = heightStartIndex; j < heightLimit; j++)
+                        {
+                            System.Drawing.Color pixel = clonedImage.GetPixel(i, j);
+                            strBuilder.AppendLine(String.Format("{0},{1},{2},{3},{4},{5}", i, j, pixel.R, pixel.G, pixel.B, pixel.A));
+                            rShades[pixel.R] = rShades[pixel.R] + 1;
+                            gShades[pixel.G] = gShades[pixel.G] + 1;
+                            bShades[pixel.B] = bShades[pixel.B] + 1;
+                        }
+                    }
+                });
+
+            SaveCsv(path, strBuilder);
+
+            var countsStrBuilder = new StringBuilder();
+            countsStrBuilder.AppendLine("RGB COUNTS");
+            countsStrBuilder.AppendLine("RCounts, Value");
+            PrintShades(rShades, countsStrBuilder);
+            countsStrBuilder.AppendLine("GCounts, Value");
+            PrintShades(gShades, countsStrBuilder);
+            countsStrBuilder.AppendLine("BCounts, Value");
+            PrintShades(bShades, countsStrBuilder);
+
+            SaveCsv(path, countsStrBuilder, "rgba.csv");
+        }
+
+        public void PrintShades(Dictionary<int, int> shades, StringBuilder stringBuilder)
+        {
+            foreach (var el in shades)
+            {
+                stringBuilder.AppendLine(String.Format("{0}, {1}", el.Key, el.Value));
+            }
         }
 
         public void SaveCsv(string path, StringBuilder strBuilder, string fileName = "data.csv")
@@ -111,9 +196,20 @@ namespace ImageProcessing
             NotProcessedImageWidth.Text = pixelWidth.ToString() + "px";
             CurrentState.Text = "is loading...";
 
+            string threadsNumberText = ThreadsNumber.Text;
+
             await Task.Run(() =>
             {
-                CreateShadesCsvWithoutTreads(path);
+                int threadsNumber;
+                if (int.TryParse(threadsNumberText, out threadsNumber))
+                {
+                    CreateShadesWithMultipleThreads(path, threadsNumber);
+                }
+                else
+                {
+                    CreateShadesCsvWithoutThreads(path);
+                }
+
 
                 Dispatcher.Invoke(() =>
                 {
